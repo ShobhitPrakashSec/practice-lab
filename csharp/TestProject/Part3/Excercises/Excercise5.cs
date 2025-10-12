eaed55c925ee2297e5b5710deea1afeb380b273b
Shobhit Prakash <shobhit.prakash.00@gmail.com>
2025-10-12 22:15:30 +0530

    deleted:    csharp/TestProject/Excercise5.cs new file:   csharp/TestProject/Part3/Excercises/Excercise5.cs moved from csharp/TestProject/Excercise5.cs new file:   csharp/TestProject/Part3/Excercises/Excercise6.cs

---
+++ csharp/TestProject/Part3/Excercises/Excercise5.cs
@@ -0,0 +1,58 @@
+﻿// // Practice Excercise 5
+// // For Loo[]
+
+// // Basic loop
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// // Reverse loop
+// for (int i = 10; i > 0; i--)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+// // Experiment with different increments 
+// for (int i = 0; i <= 20; i += 3)
+// {
+//     Console.WriteLine(i);
+// }
+
+// // Use the break keyword to break the iteration statemen
+// for (int i = 0; i < 20 ; i++)
+// {
+//     Console.WriteLine(i);
+//     if (i == 10)
+//         break;
+// }
+
+// // Loop through each element of an array
+// string[] names = ["Alice", "Bob", "Charlie", "David"];
+// for (int i = names.Length - 1; i >= 0; i--)
+// {
+//     Console.WriteLine(names[i]);
+// }
+
+// // Limitation of foreach loop
+// string[] fruits = ["Apple", "Banana", "Cherry"];
+// foreach (string fruit in fruits)
+// {
+//     Console.WriteLine(fruit);
+//     // fruit = "Orange"; // This will cause a compile-time error
+//     // Can't do this:
+//     // if (fruit == "David") fruit = "Sammy"; //  error CS1656: Cannot assign to 'fruit' because it is a 'foreach iteration variable'
+// }
+
+// // overcoming the limitation of foreach loop
+// string[] fruits = ["Apple", "Banana", "Cherry"];
+// for (int i = 0; i < fruits.Length; i++)
+// {
+//     if (fruits[i] == "Banana")
+//         fruits[i] = "Mango"; // This is allowed
+// }
+// foreach (string fruit in fruits)
+// {
+//     Console.WriteLine(fruit);
+// }
---
+++ csharp/TestProject/Part3/Excercises/Excercise6.cs
@@ -0,0 +1,20 @@
+﻿// // Excercise 6: a challenge activity using for and if statements
+// // https://learn.microsoft.com/en-gb/training/modules/csharp-for/3-exercise-challenge-for-statements
+
+// for ( int i =1; i <= 100; i++)
+// {
+//     Console.Write(i);
+//     if (i % 3 == 0 && i % 5 == 0)
+//     {
+//         Console.WriteLine(" - FizzBuzz");
+//     }
+//     else if (i % 3 == 0)
+//     {
+//         Console.WriteLine(" - Fizz");
+//     }
+//     else if (i % 5 == 0)
+//     {
+//         Console.WriteLine(" - Buzz");
+//     }
+//     else Console.WriteLine("");
+// }
