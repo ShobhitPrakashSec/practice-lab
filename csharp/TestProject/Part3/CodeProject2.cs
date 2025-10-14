a70ee2eba71d542e6d3d69ec3bb9afc8264f967f
Shobhit Prakash <shobhit.prakash.00@gmail.com>
2025-10-14 10:51:45 +0530

    modified:   csharp/TestProject/Part3/CodeProject2.cs

--- csharp/TestProject/Part3/CodeProject2.cs
+++ csharp/TestProject/Part3/CodeProject2.cs
@@ -1,27 +1,27 @@
-﻿// https://learn.microsoft.com/en-gb/training/modules/csharp-do-while/5-exercise-challenge-differentiate-while-do-statements
-// Code project 2 - write code that validates string input
+﻿// // https://learn.microsoft.com/en-gb/training/modules/csharp-do-while/5-exercise-challenge-differentiate-while-do-statements
+// // Code project 2 - write code that validates string input
 
-string? userInput;
-bool isValidInput = false;
-string roleName;
+// string? userInput;
+// bool isValidInput = false;
+// string roleName;
 
-Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+// Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
 
-do
-{
-    userInput = Console.ReadLine();
+// do
+// {
+//     userInput = Console.ReadLine();
 
-    if(userInput != null)
-    {
-        roleName = userInput.Trim().ToLower();
-        if (roleName == "administrator" || roleName == "manager" || roleName == "user")
-        {
-            Console.WriteLine($"Your input value {roleName} has been accepted.");
-            isValidInput = true;
-        }
-        else
-            Console.WriteLine($"The role name that you entered, ({roleName}) is not valid. Enter your role name (Administrator, Manager, or User)");
-    }
+//     if(userInput != null)
+//     {
+//         roleName = userInput.Trim().ToLower();
+//         if (roleName == "administrator" || roleName == "manager" || roleName == "user")
+//         {
+//             Console.WriteLine($"Your input value {roleName} has been accepted.");
+//             isValidInput = true;
+//         }
+//         else
+//             Console.WriteLine($"The role name that you entered, ({roleName}) is not valid. Enter your role name (Administrator, Manager, or User)");
+//     }
 
 
-} while (!isValidInput);
+// } while (!isValidInput);
