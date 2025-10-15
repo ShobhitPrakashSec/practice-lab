a787c53997054076149ce00a9ed00aaa38b4cd83
Shobhit Prakash <shobhit.prakash.00@gmail.com>
2025-10-15 21:26:37 +0530

    modified:   csharp/TestProject/Part3/CodeProject3.cs comment out

--- csharp/TestProject/Part3/CodeProject3.cs
+++ csharp/TestProject/Part3/CodeProject3.cs
@@ -1,23 +1,23 @@
-﻿// https://learn.microsoft.com/en-gb/training/modules/csharp-do-while/5-exercise-challenge-differentiate-while-do-statements
-// Code project 3 - Write code that processes the contents of a string array
+﻿// // https://learn.microsoft.com/en-gb/training/modules/csharp-do-while/5-exercise-challenge-differentiate-while-do-statements
+// // Code project 3 - Write code that processes the contents of a string array
 
-string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
+// string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
 
-int periodLocation;
+// int periodLocation;
 
-for (int i = 0; i < myStrings.Length; i++)
-{
-    string myString = myStrings[i];
-    periodLocation = myString.IndexOf('.');
-    string mySentence;
+// for (int i = 0; i < myStrings.Length; i++)
+// {
+//     string myString = myStrings[i];
+//     periodLocation = myString.IndexOf('.');
+//     string mySentence;
 
-    while(periodLocation != -1)
-    {
-        mySentence = myString[..periodLocation];
-        myString = myString[(periodLocation + 1)..].TrimStart();
-        periodLocation = myString.IndexOf('.');
-        Console.WriteLine(mySentence);
-    }
+//     while(periodLocation != -1)
+//     {
+//         mySentence = myString[..periodLocation];
+//         myString = myString[(periodLocation + 1)..].TrimStart();
+//         periodLocation = myString.IndexOf('.');
+//         Console.WriteLine(mySentence);
+//     }
 
-    Console.WriteLine(myString);
-}
+//     Console.WriteLine(myString);
+// }
