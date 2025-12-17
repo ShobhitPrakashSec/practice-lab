52e158b4bb358cd5e19df5adb5b79296c7a5aaa4
Shobhit Prakash <shobhit.prakash.00@gmail.com>
2025-12-17 10:43:48 +0530

    new file:   csharp/TestProject/Part4/Exercises/Exercise10.cs Discover integral types

---
+++ csharp/TestProject/Part4/Exercises/Exercise10.cs
@@ -0,0 +1,19 @@
+/* 
+// Exercise - Discover integral types
+// https://learn.microsoft.com/en-gb/training/modules/csharp-choose-data-type/3-exercise-integral-types
+
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte: {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short: {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int: {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long: {long.MinValue} to {long.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+
+Console.WriteLine($"byte: {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort: {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint: {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong: {ulong.MinValue} to {ulong.MaxValue}");
+*/
