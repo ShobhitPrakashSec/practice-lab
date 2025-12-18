68f6d1e1d113cb025e96ef4b8a703251fdbf3ae0
Shobhit Prakash <shobhit.prakash.00@gmail.com>
2025-12-18 18:26:21 +0530

    new file:   csharp/TestProject/Part4/Exercises/Exercise15.cs Discovered Sort() and Reverse()

---
+++ csharp/TestProject/Part4/Exercises/Exercise15.cs
@@ -0,0 +1,21 @@
+/*
+// Exercise - Discover Sort() and Reverse()
+// https://learn.microsoft.com/en-gb/training/modules/csharp-arrays-operations/2-exercise-sort-reverse
+
+string[] pallets = ["B14", "A11", "B12", "A13"];
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+*/
